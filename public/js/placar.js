30638bb29b447810e5b6794f1cf87dff37a8b260
Claudio <claudio.santos57@fatec.sp.gov.br>
2020-03-31 10:22:53 -0300

    adicionando ocultação de placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Claudio"
@@ -33,3 +35,8 @@ function removeLinha() {
     event.preventDefault();
     $(this).parent().parent().remove();
 }
+
+function mostraPlacar() {
+    $(".placar").slideToggle(600);
+}
+
